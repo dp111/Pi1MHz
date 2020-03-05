6482fae6f17fb0d249942334a35c8f30a2c500cd
dp111 <dp111@users.noreply.github.com>
2020-03-05 22:25:20 +0000

    Fix bss clear alignment and move code out of arm-start.S

--- src/rpi/arm-start.h
+++ src/rpi/arm-start.h
@@ -7,18 +7,15 @@
 
 #define _enable_interrupts() {asm volatile ("CPSIE if");}
 
-extern void _set_interrupts( int cpsr );
-
 #define _disable_interrupts() {asm volatile ("CPSID if");}
 
 #if defined(RPI2) || defined(RPI3)
     #define _data_memory_barrier() {asm volatile ("dmb");}
 #else
+
     extern void _data_memory_barrier();
 #endif
 
 extern unsigned int _get_cpsr();
-
-extern unsigned int _get_stack_pointer();
 
 #endif
