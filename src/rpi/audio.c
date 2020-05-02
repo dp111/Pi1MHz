99dd553f096cf2b8710d5310b97eadda19926733
dp111 <dp111@users.noreply.github.com>
2020-05-02 12:42:59 +0100

    Filename changes

---
+++ src/rpi/audio.c
@@ -0,0 +1,136 @@
+#include "cache.h"
+#include "arm-start.h"
+#include "systimer.h"
+#include "audio.h"
+#include "rpi.h"
+
+// NB Buffer size must be a multiple of 32 bytes
+// buffer contains both left and right sample words
+#define DMA_BUFFER_SIZE 512
+
+struct bcm2708_dma_cb {
+   uint32_t info;
+   uint32_t src;
+   uint32_t dst;
+   uint32_t length;
+   uint32_t stride;
+   uint32_t next;
+   uint32_t pad[2];
+   uint32_t buffer[DMA_BUFFER_SIZE];
+};
+
+NOINIT_SECTION static __attribute__ ((aligned (0x20))) struct bcm2708_dma_cb dma_cb_data[2];
+
+/*
+buffer state dma
+
+00 under run fill buffer other buffer
+01 fill buffer other buffer
+10 nop
+11 clear buffer bit as DMA is using it.
+*/
+
+static uint32_t buffer_state;
+static uint32_t *next_buffer;
+
+size_t rpi_audio_buffer_free_space()
+{
+   if ((RPI_DMA5Base->SRC_ADR) < (((uint32_t)&dma_cb_data[1].buffer[0]) | GPU_BASE))
+   {
+      // DMA is in first buffer
+      switch (buffer_state)
+      {
+      case 0://next_buffer = (uint32_t *) &dma_cb_data[1].buffer[0]; break;
+      case 1:buffer_state = 0; next_buffer = (uint32_t *)&dma_cb_data[1].buffer[0]; break;
+      case 2:return 0;
+      case 3:buffer_state = 2; return 0; break;
+      }
+   } else
+   {
+      // DMA is in second buffer
+      switch (buffer_state)
+      {
+      case 0://next_buffer = (uint32_t *)&dma_cb_data[0].buffer[0]; break;
+      case 2:buffer_state = 0; next_buffer = (uint32_t *)&dma_cb_data[0].buffer[0]; break;
+      case 1:return 0;
+      case 3:buffer_state = 1; return 0;break;
+      }
+   }
+   return (sizeof(dma_cb_data[0].buffer)/sizeof(uint32_t));
+}
+
+uint32_t * rpi_audio_buffer_pointer()
+{
+   return next_buffer;
+}
+
+void rpi_audio_samples_written(void)
+{
+   if (next_buffer<&dma_cb_data[1].buffer[0])
+      buffer_state |= 1;
+   else
+      buffer_state |= 2;
+   // make sure the buffer is written out of cache
+   _clean_cache_area(next_buffer, sizeof(dma_cb_data[0].buffer));
+}
+
+static void init_dma_buffer(size_t buf, uint32_t buffer_init)
+{
+   dma_cb_data[buf].info = BCM2708_DMA_PER_MAP(5) | BCM2708_DMA_S_WIDTH | BCM2708_DMA_S_INC | BCM2708_DMA_D_DREQ | BCM2708_DMA_WAIT_RESP;
+   dma_cb_data[buf].src = ((uint32_t)&dma_cb_data[buf].buffer[0]) | GPU_BASE ;
+   dma_cb_data[buf].dst = ((uint32_t)(&RPI_PWMBase->PWM_FIFO) & 0x00ffffff) | PERIPHERAL_BASE_GPU; // physical address of fifo
+   dma_cb_data[buf].length = sizeof(dma_cb_data[buf].buffer);
+   dma_cb_data[buf].stride = 0;
+   dma_cb_data[buf].next = (uint32_t)&dma_cb_data[(buf+1)%2].info | GPU_BASE;
+   dma_cb_data[buf].pad[0] = 0;
+   dma_cb_data[buf].pad[1] = 0;
+   for (size_t i=0; i<sizeof(dma_cb_data[buf].buffer)/sizeof(uint32_t); i++)
+       dma_cb_data[buf].buffer[i] = buffer_init;
+   buffer_state |= 1<<buf;
+   _clean_cache_area(&dma_cb_data[buf], sizeof(dma_cb_data));
+}
+
+// return the sample range
+uint32_t rpi_audio_init(uint32_t samplerate)
+{
+   uint32_t audio_range = 500000000 / (2 * samplerate);
+
+   RPI_CLKBase->PWM_CTL = PM_PASSWORD | BCM2835_PWMCLK_CNTL_KILL;
+   RPI_PWMBase->PWM_CONTROL = 0;
+
+   // samplerate = 500000000 / 2 / range
+
+   // Bits 0..11 Fractional Part Of Divisor = 0, Bits 12..23 Integer Part Of Divisor = 2
+
+   RPI_CLKBase->PWM_DIV = PM_PASSWORD | (0x2000);
+   RPI_CLKBase->PWM_CTL = PM_PASSWORD | BCM2835_PWMCLK_CNTL_ENABLE | BCM2835_PWMCLK_CNTL_PLLD ;
+
+   usleep(1);
+
+   RPI_PWMBase->PWM0_RANGE = audio_range;
+   RPI_PWMBase->PWM1_RANGE = audio_range;
+
+   init_dma_buffer(0,audio_range>>1);
+   init_dma_buffer(1,audio_range>>1);
+
+   usleep(1);
+
+   RPI_PWMBase->PWM_DMAC = PWMDMAC_ENAB | PWMDMAC_THRSHLD;
+
+   // it feals as that we should have | BCM2835_PWM0_REPEATFF | BCM2835_PWM1_REPEATFF  enabled
+   //but this appears to half the output frequency.  May be we need to set up PWMDMAC_THRSHLD differently
+   RPI_PWMBase->PWM_CONTROL = BCM2835_PWM1_USEFIFO | BCM2835_PWM1_ENABLE |
+                              BCM2835_PWM0_USEFIFO | BCM2835_PWM0_ENABLE | BCM2735_PWMx_CLRF ;
+
+   RPI_DMABase->Enable = 1<<5; // enable DMA 5
+
+   RPI_DMA5Base->CS = BCM2708_DMA_RESET;
+   usleep(10);
+   RPI_DMA5Base->CS = BCM2708_DMA_INT | BCM2708_DMA_END;
+   RPI_DMA5Base->ADDR = (uint32_t)&dma_cb_data[0].info | GPU_BASE;
+   RPI_DMA5Base->Debug = 7; // clear debug error flags
+   usleep(10);
+   RPI_DMA5Base->CS = 0x10880000 | BCM2708_DMA_ACTIVE;  // go, mid priority, wait for outstanding writes
+
+   return audio_range;
+}
---
+++ src/rpi/audio.h
@@ -0,0 +1,140 @@
+
+#ifndef _BCM2708_AUDIO_H
+#define _BCM2708_AUDIO_H
+
+#include "base.h"
+#include <stddef.h>
+
+#define PWM_BASE          (PERIPHERAL_BASE + 0x20C000) /* PWM controller */
+#define CLOCK_BASE        (PERIPHERAL_BASE + 0x101000)
+
+#define DMA_CONTROLLER_BASE (PERIPHERAL_BASE + 0x007000)
+
+typedef struct
+{
+   rpi_reg_rw_t PWM_CONTROL;
+   rpi_reg_rw_t PWM_STATUS;
+   rpi_reg_rw_t PWM_DMAC;
+   rpi_reg_ro_t reserved1;
+   rpi_reg_rw_t PWM0_RANGE;
+   rpi_reg_rw_t PWM0_DATA;
+   rpi_reg_rw_t PWM_FIFO;
+   rpi_reg_ro_t reserved2;
+   rpi_reg_rw_t PWM1_RANGE;
+   rpi_reg_rw_t PWM1_DATA;
+} rpi_pwm_t;
+
+static rpi_pwm_t* const RPI_PWMBase = (rpi_pwm_t*) PWM_BASE;
+
+typedef struct
+{
+   rpi_reg_ro_t reserved1[40];
+   rpi_reg_rw_t PWM_CTL;
+   rpi_reg_rw_t PWM_DIV;
+} rpi_clk_t;
+
+static rpi_clk_t* const RPI_CLKBase = (rpi_clk_t*) CLOCK_BASE;
+
+#define PM_PASSWORD 0x5A000000
+
+#define BCM2835_PWMCLK_CNTL_OSCILLATOR 0x01
+#define BCM2835_PWMCLK_CNTL_PLLA 0x04
+#define BCM2835_PWMCLK_CNTL_PLLD 0x06
+#define BCM2835_PWMCLK_CNTL_KILL (1<<5)
+#define BCM2835_PWMCLK_CNTL_ENABLE (1<<4)
+
+#define PWMDMAC_ENAB	(1UL<<31)
+#define PWMDMAC_THRSHLD	((4<<8)|(4<<0))
+
+#define BCM2835_PWM1_MS_MODE    0x8000  /*  Run in MS mode                   */
+#define BCM2835_PWM1_USEFIFO    0x2000  /*  Data from FIFO                   */
+#define BCM2835_PWM1_REVPOLAR   0x1000  /* Reverse polarity             */
+#define BCM2835_PWM1_OFFSTATE   0x0800  /* Ouput Off state             */
+#define BCM2835_PWM1_REPEATFF   0x0400  /* Repeat last value if FIFO empty   */
+#define BCM2835_PWM1_SERIAL     0x0200  /* Run in serial mode             */
+#define BCM2835_PWM1_ENABLE     0x0100  /* Channel Enable             */
+
+#define BCM2735_PWMx_CLRF       0x0040  /* clear FIFO */
+
+#define BCM2835_PWM0_MS_MODE    0x0080  /* Run in MS mode             */
+#define BCM2835_PWM0_USEFIFO    0x0020  /* Data from FIFO             */
+#define BCM2835_PWM0_REVPOLAR   0x0010  /* Reverse polarity             */
+#define BCM2835_PWM0_OFFSTATE   0x0008  /* Ouput Off state             */
+#define BCM2835_PWM0_REPEATFF   0x0004  /* Repeat last value if FIFO empty   */
+#define BCM2835_PWM0_SERIAL     0x0002  /* Run in serial mode             */
+#define BCM2835_PWM0_ENABLE     0x0001  /* Channel Enable             */
+
+#define BCM2835_BERR  0x100
+#define BCM2835_GAPO4 0x80
+#define BCM2835_GAPO3 0x40
+#define BCM2835_GAPO2 0x20
+#define BCM2835_GAPO1 0x10
+#define BCM2835_RERR1 0x8
+#define BCM2835_WERR1 0x4
+#define BCM2835_EMPT1 0x2
+#define BCM2835_FULL1 0x1
+
+/* DMA CS Control and Status bits */
+#define BCM2708_DMA_ACTIVE	(1 << 0)
+#define BCM2708_DMA_INT		(1 << 2)
+#define BCM2708_DMA_ISPAUSED	(1 << 4)  /* Pause requested or not active */
+#define BCM2708_DMA_ISHELD	(1 << 5)  /* Is held by DREQ flow control */
+#define BCM2708_DMA_ERR		(1 << 8)
+#define BCM2708_DMA_ABORT	(1 << 30) /* stop current CB, go to next, WO */
+#define BCM2708_DMA_RESET	(1UL << 31) /* WO, self clearing */
+
+/* DMA control block "info" field bits */
+#define BCM2708_DMA_INT_EN	(1 << 0)
+#define BCM2708_DMA_TDMODE	(1 << 1)
+#define BCM2708_DMA_WAIT_RESP	(1 << 3)
+#define BCM2708_DMA_D_INC	(1 << 4)
+#define BCM2708_DMA_D_WIDTH	(1 << 5)
+#define BCM2708_DMA_D_DREQ	(1 << 6)
+#define BCM2708_DMA_S_INC	(1 << 8)
+#define BCM2708_DMA_S_WIDTH	(1 << 9)
+#define BCM2708_DMA_S_DREQ	(1 << 10)
+
+#define	BCM2708_DMA_BURST(x)	(((x)&0xf) << 12)
+#define	BCM2708_DMA_PER_MAP(x)	((x) << 16)
+#define	BCM2708_DMA_WAITS(x)	(((x)&0x1f) << 21)
+
+#define BCM2708_DMA_DREQ_EMMC	11
+#define BCM2708_DMA_DREQ_SDHOST	13
+
+typedef struct
+{
+   rpi_reg_rw_t CS;
+   rpi_reg_rw_t ADDR;// write address of a bcm2708_dma_cb here
+/* the current control block appears in the following registers - read only */
+   rpi_reg_ro_t INFO;
+   rpi_reg_ro_t SRC_ADR;
+   rpi_reg_ro_t DES_ADR;
+   rpi_reg_ro_t TX_LEN;
+   rpi_reg_ro_t STRIDE;
+   rpi_reg_ro_t NEXTCB;
+   rpi_reg_rw_t Debug;
+} rpi_dmax_t;
+
+static rpi_dmax_t* const RPI_DMA5Base = (rpi_dmax_t*) (DMA_CONTROLLER_BASE + 0x500);
+
+typedef struct
+{
+   rpi_reg_rw_t Int_Status;
+   rpi_reg_ro_t reserved1[3];
+   rpi_reg_rw_t Enable;
+} rpi_dma_t;
+
+static rpi_dma_t* const RPI_DMABase = (rpi_dma_t*) (DMA_CONTROLLER_BASE + 0xFE0);
+
+#define BCM2708_DMA_TDMODE_LEN(w, h) ((h) << 16 | (w))
+
+// Missing from original kernel file:
+#define BCM2708_DMA_END				(1<<1)
+#define BCM2708_DMA_NO_WIDE_BURSTS	(1<<26)
+
+size_t rpi_audio_buffer_free_space();
+uint32_t * rpi_audio_buffer_pointer();
+void rpi_audio_samples_written();
+uint32_t rpi_audio_init(uint32_t samplerate );
+
+#endif
