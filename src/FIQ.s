0fb5d16750c61ebe5e57cf4c6bc08b07d6693317
dp111 <dp111@users.noreply.github.com>
2020-05-31 08:23:36 +0100

    Improve performance

--- src/FIQ.s
+++ src/FIQ.s
@@ -14,16 +14,17 @@
 #define FIQ_SETUP_CODE 1
 .macro FIQ_SETUP_M
    LDR     R12,= (PERIPHERAL_BASE + 0x00B844)
+   mov     R12,#0
    LDR     r11,= Pi1MHz_VPU_RETURN
    mov     r10, # ADDRBUS_MASK>>ADDRBUS_SHIFT
    orr     r10,r10,#NPCFC_MASK>>ADDRBUS_SHIFT
+   mov      r10, r10 , LSL # 2
 .endm
 
-.macro DMB_MACRO
+.macro DMB_MACRO reg
 #if (__ARM_ARCH == 6 )
    // dmb   // Only needed on ArmV6 systems
-    mov      r9, #0
-    mcr      p15, 0, r9, c7, c10, 5
+    mcr      p15, 0, \reg, c7, c10, 5
 #endif
 
 .endm
@@ -36,24 +37,33 @@
 //  r13 stack
 //  r14 return address
 FIQstart:
-   DMB_MACRO
-
-   LDR      r8, [r11]       // get data posted going of chip
-   DMB_MACRO                // Stall
-   LDR      r9, [R12]       // read door bell to ack
-
+   DMB_MACRO r12            // R12 is defined to be zero
+                            // This finshes any outstanding read that might be inflight
+                            // e.g. from the foreground task
+   LDR      r8, [r11]       // get data posted from the VPU.
+   
+// Stall as going off chip 
+// so do something a bit useful 
+   orr      r12,r12,#(PERIPHERAL_BASE + 0x00B844) & 0xff000000
+   orr      r12,r12,#(PERIPHERAL_BASE + 0x00B844) & 0x00ff0000
+   orr      r12,r12,#(PERIPHERAL_BASE + 0x00B844) & 0x0000ff00
+   orr      r12,r12,#(PERIPHERAL_BASE + 0x00B844) & 0x000000ff
+   
    tst      r8, # RNW_MASK
-   and      r9, r10, r8, LSR # ADDRBUS_SHIFT // isolate address bus and fred or jim
+   
+   LDR      r12,[R12]       // read door bell to ack 
 
-   orrne    r9, r9, # Pi1MHz_MEM_RNW     // set read flag ready for call back table
+   and      r9, r10, r8, LSR # ADDRBUS_SHIFT - 2 // isolate address bus and fred or jim
 
-   mov      r9, r9 , LSL # 2
+   orrne    r9, r9, # Pi1MHz_MEM_RNW<<2     // set read flag ready for call back table
+
    ldr      r9, [r9, #Pi1MHz_CB_BASE]    // load call back pointer
 
 //stall
+   mov      r12, #0
 
    movs     r9, r9
-
+   DMB_MACRO r12           // check doorbell ack has actually left the core. 
    subeqs   pc, lr,#4   // no callback
 
    push    {r0-r3,r12, r14}
