6e8bb365e87afe336ece6b73ab1227354712f7d1
dp111 <19616418+dp111@users.noreply.github.com>
2022-06-02 14:58:29 +0100

    4K page support not needed in this project

--- src/rpi/cache.h
+++ src/rpi/cache.h
@@ -3,11 +3,9 @@
 #ifndef CACHE_H
 #define CACHE_H
 
-/* The first 2MB of memory is mapped at 4K pages so the 6502 Co Pro
- can play tricks with banks selection */
-#define NUM_4K_PAGES 512
-
 void _clean_cache_area(void *start, unsigned int length);
+
+void _invalidate_cache_area(void * start, unsigned int length);
 
 void map_4k_page(unsigned int logical, unsigned int physical);
 
