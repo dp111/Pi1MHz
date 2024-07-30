c73e28b76bcb67eafd4ae289f92487f5231eec6b
dp111 <19616418+dp111@users.noreply.github.com>
2024-07-30 11:00:34 +0100

    More tidying of Videcore  code

--- vidcore/build.sh
+++ vidcore/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ../tools/vasm/vasmvidcore_std  -Fbin -L Pi1MHzvc.lst -o Pi1MHzvc.asm Pi1MHzvc.s
 rm -f ../src/Pi1MHzvc.c
+echo -n "__attribute__((aligned(256))) const " >>../src/Pi1MHzvc.c
 xxd -i Pi1MHzvc.asm >> ../src/Pi1MHzvc.c
