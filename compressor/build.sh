8b3368c540aa3f19394829432599479526674028
dp111 <19616418+dp111@users.noreply.github.com>
2025-03-09 20:26:33 +0000

    update frame builder script

--- compressor/build.sh
+++ compressor/build.sh
@@ -1,65 +1,67 @@
 #!/bin/sh
 
 
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <path_to_tbc_file>"
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <path_to_tbc_file> <start_frame/4096>"
   exit 1
 fi
 
-#cleanup
-rm frame.yuv
-rm framecor.lz4
-rm framecor.lz
-
-rm index.dat
-rm frames*.dat
 
 tbc_file=$1
 length=0
+start=$2
+start=$((start*4096))
+last=$((start+4095))
+if [ $((last)) -gt 54000 ]; then
+    last=54000
+fi
+divided_i=$((start / 4096))
+#cleanup
+rm frame$divided_i.yuv
+rm framecor$divided_i.lz4
+rm framecor$divided_i.lz
 
-for i in $(seq 1 54000)
+rm index$divided_i.dat
+rm frames$divided_i.dat
+
+for i in $(seq $start $last)
 do
 
- /mnt/c/Archlinux/ld-decode/tools/ld-chroma-decoder/ld-chroma-decoder --decoder transform3d -p y4m -s $i -l 1 -q "$tbc_file"  | ffmpeg -hide_banner -loglevel error -i - -flags +ilme+ildct -pix_fmt yuv422p -color_primaries bt470bg -color_trc bt709 -colorspace bt470bg -color_range tv -vf setdar=4/3,setfield=tff,scale=768x576 -c:v rawvideo frame.yuv
+ld-decode/tools/ld-chroma-decoder/ld-chroma-decoder --decoder transform3d -p y4m -s $i -l 1 -q "$tbc_file"  | ffmpeg -hide_banner -loglevel error -i - -flags +ilme+ildct -pix_fmt yuv422p -color_primaries bt470bg -color_trc bt709 -colorspace bt470bg -color_range tv -vf setdar=4/3,setfield=tff,scale=768x576 -c:v rawvideo "frame$divided_i.yuv"
 
 #truncate :
 ##dd if=frame.yuv iflag=skip_bytes,count_bytes,fullblock bs=4096 skip=36 count=884736 of=framecor.yuv
 
 #compress
-lz4 -12 frame.yuv framecor.lz4
+lz4 -12 "frame$divided_i.yuv" "framecor$divided_i.lz4"
 
 #there is a 7 byte header which we don't need so remove that
 
-dd if=framecor.lz4 iflag=skip_bytes,count_bytes,fullblock bs=4096 skip=7 of=framecor.lz status=none
+dd if="framecor$divided_i.lz4" iflag=skip_bytes,count_bytes,fullblock bs=4096 skip=7 of="framecor$divided_i.lz" status=none
 
-divided_i=$((i / 4096))
-
-cat framecor.lz >>"frames$divided_i.dat"
+cat "framecor$divided_i.lz" >>"frames$divided_i.dat"
 
  # Append the length to index.dat as a uint32
- # printf "%08x" $length | xxd -r -p >> index.dat
+# printf "%08x" $length | xxd -r -p >> indexa.dat
 
   # Append the length to index.dat as a uint32 in little-endian format
  # printf "%08x" $length | xxd -r -p | xxd -e -r >> index.dat
 
   # Append the length to index.dat as a uint32 in little-endian format
-  printf "%08x" $file_length | sed 's/../& /g' | awk '{print $4 $3 $2 $1}' | xxd -r -p >> index.dat
+  printf "%08x" $length | sed 's/../& /g' | awk '{print $4 $3 $2 $1}' | xxd -r -p >> index$divided_i.dat
 
 # Get the length of framecor.lz
-file_length=$(stat -c%s "framecor.lz")
+file_length=$(stat -c%s "framecor$divided_i.lz")
 
 length=$((length + file_length))
 
 echo "frame $i length: $file_length total length: $length"
-# Reset length every 4096 iterations
-if [ $((i % 4096)) -eq 0 ]; then
-    length=0
-    echo "frame $i done"
-fi
 
 #cleanup
-rm frame.yuv
-rm framecor.lz4
-rm framecor.lz
+rm frame$divided_i.yuv
+rm framecor$divided_i.lz4
+rm framecor$divided_i.lz
 
 done
+
+echo "frame $i done"
