7dbb5688ffb8d73606fd3a10fbd7ad1298ff95ed
dp111 <19616418+dp111@users.noreply.github.com>
2026-05-26 22:18:19 +0100

    use strlcpy to prevent any possiblity of buffer overflow

--- src/Pi1MHz.c
+++ src/Pi1MHz.c
@@ -248,7 +248,7 @@ void Pi1MHz_LED(int led)
 static char* putstring(char *ram, char term, const char *string)
 {
    size_t length = strlen(string);
-   strcpy(ram, string);
+   strlcpy(ram, string, PAGE_SIZE);
    ram += length;
    if (term == '\n')
    {
