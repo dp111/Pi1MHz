24d620f24c3552ea5db78b088776805696fa567d
dp111 <19616418+dp111@users.noreply.github.com>
2026-05-26 22:22:46 +0100

    Add back in old TAG system for now.

--- src/rpi/mailbox.c
+++ src/rpi/mailbox.c
@@ -1,7 +1,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <inttypes.h>
 #include "rpi.h"
 #include "mailbox.h"
 #include "cache.h"
@@ -52,6 +52,200 @@ static uint32_t RPI_Mailbox0Read( mailbox0_channel_t channel )
     return value >> 4;
 }
 
+
+void RPI_PropertyInit( void )
+{
+    /* First available data slot */
+   pt_index = 2;
+}
+
+/**
+    @brief Add a property tag to the current tag list. Data can be included. All data is uint32_t
+    @param tag
+*/
+void RPI_PropertyAddTag( rpi_mailbox_tag_t tag, ... )
+{
+    int offset;
+    int num_colours;
+    va_list vl;
+    va_start( vl, tag );
+
+    pt[pt_index++] = tag;
+
+    switch( tag )
+    {
+        case TAG_GET_FIRMWARE_VERSION:
+        case TAG_GET_BOARD_MODEL:
+        case TAG_GET_BOARD_REVISION:
+        case TAG_GET_BOARD_MAC_ADDRESS:
+        case TAG_GET_BOARD_SERIAL:
+        case TAG_GET_ARM_MEMORY:
+        case TAG_GET_VC_MEMORY:
+        case TAG_GET_DMA_CHANNELS:
+            /* Provide an 8-byte buffer for the response */
+            pt[pt_index++] = 8;
+            pt[pt_index++] = 0; /* Request */
+            pt_index += 2;
+            break;
+
+        case TAG_GET_CLOCKS:
+        case TAG_GET_COMMAND_LINE:
+            /* Provide a 1024-byte buffer */
+            pt[pt_index++] = PROP_SIZE;
+            pt[pt_index++] = 0; /* Request */
+            pt_index += PROP_SIZE >> 2;
+            break;
+
+        case TAG_GET_CLOCK_RATE:
+        case TAG_GET_MAX_CLOCK_RATE:
+        case TAG_GET_MIN_CLOCK_RATE:
+        case TAG_GET_TURBO:
+        case TAG_GET_TEMPERATURE:
+        case TAG_GET_MAX_TEMPERATURE:
+        case TAG_GET_VOLTAGE:
+        case TAG_GET_MIN_VOLTAGE:
+        case TAG_GET_MAX_VOLTAGE:
+            pt[pt_index++] = 8;
+            pt[pt_index++] = 0; /* Request */
+            pt[pt_index++] = va_arg( vl, int ); /* ClockID */
+            pt_index += 1;
+            break;
+
+        case TAG_EXECUTE_CODE:
+        case TAG_LAUNCH_VPU1:
+       // case TAG_LAUNCH_VPU1:
+            pt[pt_index++] = 28;
+            pt[pt_index++] = 0; /* Request */
+            pt[pt_index++] = va_arg( vl, int ); // Function pointer
+            pt[pt_index++] = va_arg( vl, int ); // R0
+            pt[pt_index++] = va_arg( vl, int ); // R1
+            pt[pt_index++] = va_arg( vl, int ); // R2
+            pt[pt_index++] = va_arg( vl, int ); // R3
+            pt[pt_index++] = va_arg( vl, int ); // R4
+            pt[pt_index++] = va_arg( vl, int ); // R5
+            break;
+
+        case TAG_ALLOCATE_BUFFER:
+            pt[pt_index++] = 8;
+            pt[pt_index++] = 0; /* Request */
+            pt[pt_index++] = va_arg( vl, int );
+            pt[pt_index++] = 0;
+            break;
+
+        case TAG_ENABLE_GPU:
+            pt[pt_index++] = 8;
+            pt[pt_index++] = 0; /* Request */
+            pt[pt_index++] = va_arg( vl, int ); /* 0 = disable, 1 = enable */
+            pt[pt_index++] = 0;
+            break;
+
+        case TAG_ALLOCATE_MEMORY:
+            pt[pt_index++] = 4;
+            pt[pt_index++] = 0; /* Request */
+            pt[pt_index++] = va_arg( vl, int ); /* u32: size      */
+            pt[pt_index++] = va_arg( vl, int ); /* u32: alignment */
+            pt[pt_index++] = va_arg( vl, int ); /* u32: flags     */
+            break;
+
+        case TAG_LOCK_MEMORY:
+        case TAG_RELEASE_MEMORY:
+        case TAG_UNLOCK_MEMORY:
+            pt[pt_index++] = 4;
+            pt[pt_index++] = 0; /* Request */
+            pt[pt_index++] = va_arg( vl, int ); /* u32: handle    */
+            break;
+
+        case TAG_GET_PHYSICAL_SIZE:
+        case TAG_SET_PHYSICAL_SIZE:
+        case TAG_TEST_PHYSICAL_SIZE:
+        case TAG_GET_VIRTUAL_SIZE:
+        case TAG_SET_VIRTUAL_SIZE:
+        case TAG_TEST_VIRTUAL_SIZE:
+        case TAG_GET_VIRTUAL_OFFSET:
+        case TAG_SET_VIRTUAL_OFFSET:
+            pt[pt_index++] = 8;
+            pt[pt_index++] = 0; /* Request */
+
+            if( ( tag == TAG_SET_PHYSICAL_SIZE ) ||
+                ( tag == TAG_SET_VIRTUAL_SIZE ) ||
+                ( tag == TAG_SET_VIRTUAL_OFFSET ) ||
+                ( tag == TAG_TEST_PHYSICAL_SIZE ) ||
+                ( tag == TAG_TEST_VIRTUAL_SIZE ) )
+            {
+                pt[pt_index++] = va_arg( vl, int ); /* Width */
+                pt[pt_index++] = va_arg( vl, int ); /* Height */
+            }
+            else
+            {
+                pt_index += 2;
+            }
+            break;
+
+
+        case TAG_GET_ALPHA_MODE:
+        case TAG_SET_ALPHA_MODE:
+        case TAG_GET_DEPTH:
+        case TAG_SET_DEPTH:
+        case TAG_GET_PIXEL_ORDER:
+        case TAG_SET_PIXEL_ORDER:
+        case TAG_GET_PITCH:
+            pt[pt_index++] = 4;
+            pt[pt_index++] = 0; /* Request */
+
+            if( ( tag == TAG_SET_DEPTH ) ||
+                ( tag == TAG_SET_PIXEL_ORDER ) ||
+                ( tag == TAG_SET_ALPHA_MODE ) )
+            {
+                /* Colour Depth, bits-per-pixel \ Pixel Order State */
+                pt[pt_index++] = va_arg( vl, int );
+            }
+            else
+            {
+                pt_index += 1;
+            }
+            break;
+
+        case TAG_GET_OVERSCAN:
+        case TAG_SET_OVERSCAN:
+            pt[pt_index++] = 16;
+            pt[pt_index++] = 0; /* Request */
+
+            if( ( tag == TAG_SET_OVERSCAN ) )
+            {
+                pt[pt_index++] = va_arg( vl, int ); /* Top pixels */
+                pt[pt_index++] = va_arg( vl, int ); /* Bottom pixels */
+                pt[pt_index++] = va_arg( vl, int ); /* Left pixels */
+                pt[pt_index++] = va_arg( vl, int ); /* Right pixels */
+            }
+            else
+            {
+                pt_index += 4;
+            }
+            break;
+
+       case TAG_SET_PALETTE:
+            offset = va_arg( vl, int);
+            num_colours = va_arg( vl, int);
+            pt[pt_index++] = 8 + num_colours * 4;
+            pt[pt_index++] = 0; /* Request */
+            pt[pt_index++] = offset;                   // Offset to first colour
+            pt[pt_index++] = num_colours;              // Number of colours
+            uint32_t *palette = va_arg( vl, uint32_t *);
+            for (int i = 0; i < num_colours; i++) {
+               pt[pt_index++] = palette[offset + i];
+            }
+            pt[pt_index++] = 0;
+            break;
+
+        default:
+            /* Unsupported tags, just remove the tag from the list */
+            pt_index--;
+            break;
+    }
+
+    va_end( vl );
+}
+
 rpi_mailbox_property_t* RPI_PropertyGetWord(rpi_mailbox_tag_t tag, uint32_t data)
 {
     pt_index = 2;
@@ -60,7 +254,6 @@ rpi_mailbox_property_t* RPI_PropertyGetWord(rpi_mailbox_tag_t tag, uint32_t data
     pt[pt_index++] = 0; /* Request */
     pt[pt_index++] = data;
     pt_index += 1;
-    pt[pt_index] = 0;
     RPI_PropertyProcess();
     return RPI_PropertyGet(tag);
 }
@@ -73,7 +266,6 @@ void RPI_PropertySetWord(rpi_mailbox_tag_t tag, uint32_t id, uint32_t data)
     pt[pt_index++] = 0; /* Request */
     pt[pt_index++] = id;
     pt[pt_index++] = data;
-    pt[pt_index] = 0;
     RPI_PropertyProcess();
 }
 
@@ -85,7 +277,6 @@ rpi_mailbox_property_t* RPI_PropertyGetBuffer(rpi_mailbox_tag_t tag)
     pt[pt_index++] = PROP_SIZE;
     pt[pt_index++] = 0; /* Request */
     pt_index += PROP_SIZE >> 2;
-    pt[pt_index] = 0;
     RPI_PropertyProcess();
     return RPI_PropertyGet(tag);
 }
@@ -99,7 +290,6 @@ void RPI_PropertySetBuffer(rpi_mailbox_tag_t tag, uint32_t *buffer, uint32_t len
     for (uint32_t i = 0; i < length; i++)
         pt[pt_index++] = buffer[i];
 
-    pt[pt_index] = 0;
     RPI_PropertyProcess();
 }
 
@@ -107,16 +297,16 @@ int RPI_PropertyProcess( void )
 {
     int result;
 
-#if( PRINT_PROP_DEBUG == 1 )
-    LOG_INFO( "%s Length: %d\r\n", __func__, pt[PT_OSIZE] );
-#endif
     /* Fill in the size of the buffer */
     pt[PT_OSIZE] = ( pt_index + 1 ) << 2;
     pt[PT_OREQUEST_OR_RESPONSE] = 0;
+    /* Make sure the tags are 0 terminated to end the list and update the buffer size */
+    pt[pt_index] = 0;
 
 #if( PRINT_PROP_DEBUG == 1 )
+    LOG_INFO( "%s Length: %"PRIx32"\r\n", __func__, pt[PT_OSIZE] );
     for ( int i = 0; i < (pt[PT_OSIZE] >> 2); i++ )
-        LOG_INFO( "Request: %3d %8.8X\r\n", i, pt[i] );
+        LOG_INFO( "Request: %3d %8.8"PRIx32"\r\n", i, pt[i] );
 #endif
     RPI_Mailbox0Write( MB0_TAGS_ARM_TO_VC, pt );
 
@@ -124,7 +314,7 @@ int RPI_PropertyProcess( void )
 
 #if( PRINT_PROP_DEBUG == 1 )
     for ( int i = 0; i < (pt[PT_OSIZE] >> 2); i++ )
-        LOG_INFO( "Response: %3d %8.8X\r\n", i, pt[i] );
+        LOG_INFO( "Response: %3d %8.8"PRIx32"\r\n", i, pt[i] );
 #endif
     return result;
 }
