811259f1f3b56aaa2d4e5e9a78e95c6b176667bb
dp111 <19616418+dp111@users.noreply.github.com>
2026-05-26 22:25:41 +0100

    Tidy up code

--- 6502code/6502code.asm
+++ 6502code/6502code.asm
@@ -72,6 +72,82 @@ MACRO FINDSWR
         sta    &fe30
 ENDMACRO
 
+MACRO LOADFILETOSWR filename
+    ; fopen
+    LDY #0   : STY discaccess
+    DEY      : STY discaccess+1
+               STY discaccess+2
+
+.fopenloop
+    INY
+    LDA fopenstring, Y: STA discaccess+3
+    BPL fopenloop
+    STA discaccess+4
+
+.fopencheckloop
+    LDA discaccess+4
+    BMI fopencheckloop
+    BNE pagertsjmp ; file not found
+
+    LDY #0   : STY discaccess
+    DEY      : STY discaccess+1
+               STY discaccess+2
+
+.freadsetuploop
+    INY :
+    LDA freaddata, Y: STA discaccess+3
+    CMP #255
+    BNE freadsetuploop
+    STA discaccess+4
+
+.freadcheckloop
+    LDA discaccess+4
+    BMI freadcheckloop
+    BEQ readdone
+    CMP #20
+    BNE pagertsjmp ; file open error
+
+.readdone
+    LDY #0   : STY discaccess
+             : STY discaccess+1
+    LDA #&F0 : STA discaccess+2
+
+             : STY swrpointer+1
+    LDA #&80 : STA swrpointer+2
+
+    LDA &F4
+    PHA
+    STX &F4
+    STX &FE30
+
+.copyswrloop
+    LDA discaccess+3 : .swrpointer STA &8000,Y
+    INY
+    BNE copyswrloop
+
+    LDX swrpointer+2
+    INX
+    STX swrpointer+2
+    CPX #&C0
+    BNE copyswrloop;
+
+    PLA
+    STA &F4
+    STA &FE30
+    JMP (&FFFC) ; Reset
+
+
+.fopenstring
+    EQUB 2, 0, 1 : EQUS filename : EQUB 0, 255
+
+.freaddata
+    EQUB 4, 0, &40, 0
+    EQUB 0, 0, &F0, 0
+    EQUB 0, 0, 0, 0
+    EQUB &FF
+
+ENDMACRO
+
 ; Page 0
 ; help screen
 {
@@ -128,78 +204,8 @@ ORG &FD00
 .pagertsjmp
     PAGERTS; no SWR found
 .SWRfound
-    CLC:ADC #33:JSR &FFEE
-    ; fopen
-    LDY #0   : STY discaccess
-    DEY      : STY discaccess+1
-               STY discaccess+2
-.fopenloop
-    INY
-    LDA fopenstring, Y: STA discaccess+3
-    BPL fopenloop
-    STA discaccess+4
 
-.fopencheckloop
-    LDA discaccess+4
-    BMI fopencheckloop
-    BNE pagertsjmp ; file not found
-
-    LDY #0   : STY discaccess
-    DEY      : STY discaccess+1
-               STY discaccess+2
-
-.freadsetuploop
-    LDA freaddata+1, Y: STA discaccess+3
-    INY
-    CPY #12
-    BNE freadsetuploop
-    STA discaccess+4
-
-.freadcheckloop
-    LDA discaccess+4
-    BMI freadcheckloop
-    BEQ readdone
-    CMP #20
-    BNE pagertsjmp ; file open error
-
-.readdone
-    LDY #0   : STY discaccess
-             : STY discaccess+1
-    LDA #&F0 : STA discaccess+2
-
-             : STY swrpointer+1
-    LDA #&80 : STA swrpointer+2
-
-    LDA &F4
-    PHA
-    STX &F4
-    STX &FE30
-
-.copyswrloop
-    LDA discaccess+3 : .swrpointer STA &8000,Y
-    INY
-    BNE copyswrloop
-
-    LDX swrpointer+2
-    INX
-    STX swrpointer+2
-    CPX #&C0
-    BNE copyswrloop;
-
-    PLA
-    STA &F4
-    STA &FE30
-    JMP (&FFFC) ; Reset
-
-
-.fopenstring
-    EQUB 2 , 0, 1 : EQUS "ROMS\ADFS.rom" : EQUB 0, 255
-
-.freaddata
-    EQUB 4, 0, &40, 0
-    EQUB 0, 0, &F0, 0
-    EQUB 0, 0, 0, 0
-    EQUB &FF
+    LOADFILETOSWR "ROMS/ADFS.rom"
 
     ENDBLOCK &200
 }
@@ -216,77 +222,8 @@ ORG &FD00
 .pagertsjmp
     PAGERTS; no SWR found
 .SWRfound
-    ; fopen
-    LDY #0   : STY discaccess
-    DEY      : STY discaccess+1
-               STY discaccess+2
-.fopenloop
-    INY
-    LDA fopenstring, Y: STA discaccess+3
-    BPL fopenloop
-    STA discaccess+4
 
-.fopencheckloop
-    LDA discaccess+4
-    BMI fopencheckloop
-    BNE pagertsjmp ; file not found
-
-    LDY #0   : STY discaccess
-    DEY      : STY discaccess+1
-               STY discaccess+2
-
-.freadsetuploop
-    LDA freaddata+1, Y: STA discaccess+3
-    INY
-    CPY #12
-    BNE freadsetuploop
-    STA discaccess+4
-
-.freadcheckloop
-    LDA discaccess+4
-    BMI freadcheckloop
-    BEQ readdone
-    CMP #20
-    BNE pagertsjmp ; file open error
-
-.readdone
-    LDY #0   : STY discaccess
-             : STY discaccess+1
-    LDA #&F0 : STA discaccess+2
-
-             : STY swrpointer+1
-    LDA #&80 : STA swrpointer+2
-
-    LDA &F4
-    PHA
-    STX &F4
-    STX &FE30
-
-.copyswrloop
-    LDA discaccess+3 : .swrpointer STA &8000,Y
-    INY
-    BNE copyswrloop
-
-    LDX swrpointer+2
-    INX
-    STX swrpointer+2
-    CPX #&C0
-    BNE copyswrloop;
-
-    PLA
-    STA &F4
-    STA &FE30
-    JMP (&FFFC) ; Reset
-
-
-.fopenstring
-    EQUB 2 , 0, 1 : EQUS "ROMS\MFS.rom" : EQUB 0, 255
-
-.freaddata
-    EQUB 4, 0, &40, 0
-    EQUB 0, 0, &F0, 0
-    EQUB 0, 0, 0, 0
-    EQUB &FF
+    LOADFILETOSWR "ROMS/MMFS.rom"
 
     ENDBLOCK &300
 }
@@ -302,82 +239,11 @@ ORG &FD00
 .pagertsjmp
     PAGERTS; no SWR found
 .SWRfound
-    ; fopen
-    LDY #0   : STY discaccess
-    DEY      : STY discaccess+1
-               STY discaccess+2
-.fopenloop
-    INY
-    LDA fopenstring, Y: STA discaccess+3
-    BPL fopenloop
-    STA discaccess+4
 
-.fopencheckloop
-    LDA discaccess+4
-    BMI fopencheckloop
-    BNE pagertsjmp ; file not found
-
-    LDY #0   : STY discaccess
-    DEY      : STY discaccess+1
-               STY discaccess+2
-
-.freadsetuploop
-    LDA freaddata+1, Y: STA discaccess+3
-    INY
-    CPY #12
-    BNE freadsetuploop
-    STA discaccess+4
-
-.freadcheckloop
-    LDA discaccess+4
-    BMI freadcheckloop
-    BEQ readdone
-    CMP #20
-    BNE pagertsjmp ; file open error
-
-.readdone
-    LDY #0   : STY discaccess
-             : STY discaccess+1
-    LDA #&F0 : STA discaccess+2
-
-             : STY swrpointer+1
-    LDA #&80 : STA swrpointer+2
-
-    LDA &F4
-    PHA
-    STX &F4
-    STX &FE30
-
-.copyswrloop
-    LDA discaccess+3 : .swrpointer STA &8000,Y
-    INY
-    BNE copyswrloop
-
-    LDX swrpointer+2
-    INX
-    STX swrpointer+2
-    CPX #&C0
-    BNE copyswrloop;
-
-    PLA
-    STA &F4
-    STA &FE30
-    JMP (&FFFC) ; Reset
-
-
-.fopenstring
-    EQUB 2 , 0, 1 : EQUS "ROMS\MMFSv2.rom" : EQUB 0, 255
-
-.freaddata
-    EQUB 4, 0, &40, 0
-    EQUB 0, 0, &F0, 0
-    EQUB 0, 0, 0, 0
-    EQUB &FF
+    LOADFILETOSWR "ROMS/MMFSv2.rom"
 
     ENDBLOCK &400
 }
-
-
 
 .end
 
